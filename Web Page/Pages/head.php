4b9ed265fe5fd2fce48c0a80ada9104f6bc65ebe
guilhermevpinto <guilhermevpinto@gmail.com>
2015-12-03 10:15:55 +0000

    cleaned main page html and css. footer created

--- Web Page/Pages/head.php
+++ Web Page/Pages/head.php
@@ -1,9 +1,11 @@
 <?php
  function makeHeader($title){?>
+ 
 <title><?php echo $title; ?> </title>
 <meta name="keywords" content="" />
 <meta name="description" content="" />
 <link href="../styles/pageHeaderStyle.css" rel="stylesheet" type="text/css" media="all" />
+<link href="../styles/pageFooterStyle.css" rel="stylesheet" type="text/css" media="all" />
 <?php include '../Server/startSession.php'; ?>
 
 <?php }?> 
